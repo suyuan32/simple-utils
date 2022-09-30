84092715a1dbf3d0181800fe07b4ce5590442ce7
Ryan Su <yuansu.china.work@gmail.com>
2022-09-30 17:33:29 +0800

    feat: pathx

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/suyuan32/simple-utils
+
+go 1.19
+
+require github.com/logrusorgru/aurora v2.0.3+incompatible
---
+++ pathx/path.go
@@ -0,0 +1,83 @@
+package pathx
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/logrusorgru/aurora"
+)
+
+// CreateIfNotExist creates a file if it is not exists.
+func CreateIfNotExist(file string) (*os.File, error) {
+	_, err := os.Stat(file)
+	if !os.IsNotExist(err) {
+		return nil, fmt.Errorf("%s already exist", file)
+	}
+
+	return os.Create(file)
+}
+
+// RemoveIfExist deletes the specified file if it is exists.
+func RemoveIfExist(filename string) error {
+	if !FileExists(filename) {
+		return nil
+	}
+
+	return os.Remove(filename)
+}
+
+// FileExists returns true if the specified file is exists.
+func FileExists(file string) bool {
+	_, err := os.Stat(file)
+	return err == nil
+}
+
+// RemoveOrQuit deletes the specified file if read a permit command from stdin.
+func RemoveOrQuit(filename string) error {
+	if !FileExists(filename) {
+		return nil
+	}
+
+	fmt.Printf("%s exists, overwrite it?\nEnter to overwrite or Ctrl-C to cancel...",
+		aurora.BgRed(aurora.Bold(filename)))
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+	return os.Remove(filename)
+}
+
+// FileNameWithoutExt returns a file name without suffix.
+func FileNameWithoutExt(file string) string {
+	return strings.TrimSuffix(file, filepath.Ext(file))
+}
+
+// SameFile compares the between path if the same path,
+// it maybe the same path in case case-ignore, such as:
+// /Users/go_zero and /Users/Go_zero, as far as we know,
+// this case maybe appear on macOS and Windows.
+func SameFile(path1, path2 string) (bool, error) {
+	stat1, err := os.Stat(path1)
+	if err != nil {
+		return false, err
+	}
+
+	stat2, err := os.Stat(path2)
+	if err != nil {
+		return false, err
+	}
+
+	return os.SameFile(stat1, stat2), nil
+}
+
+// MustTempDir creates a temporary directory.
+func MustTempDir() string {
+	dir, err := os.MkdirTemp("", "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return dir
+}
